8e4f97a902b00632451b41ffae1882d0fb58c503
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-11-10 12:09:24 +0100

    file upload

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var multipart = require('connect-multiparty');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+var configuration = require('./configuration');
+var manager = require('./lib/manager');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+
+// mysql
+manager.setDatabase(mysql.createPool({
+    host     : configuration.mysql.host,
+    user     : configuration.mysql.user,
+    password : configuration.mysql.password,
+    database : configuration.mysql.database
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,10 +32,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(multipart());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var api = require('../lib/api');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/report/add', function(req, res) {
+    // remove all files
+    for(var file in req.files) {
+        if (req.files.hasOwnProperty(file)) {
+            fs.unlink(req.files[file].path);
+        }
+    }
+
+    res.end();
 });
 
 module.exports = router;
